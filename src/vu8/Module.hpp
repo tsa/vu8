75075785100efd0064fda53971cc5346dc90fa89
jpike <jpike@ts-a.com>
2011-01-06 18:19:35 +0000

    use object template rather than function template for modules

--- src/vu8/Module.hpp
+++ src/vu8/Module.hpp
@@ -11,30 +11,28 @@ struct Module {
     Module& operator()(char const            *name,
                        v8::InvocationCallback callback)
     {
-        func_->PrototypeTemplate()->
-            Set(v8::String::New(name), v8::FunctionTemplate::New(callback));
+        obj_->Set(v8::String::New(name), v8::FunctionTemplate::New(callback));
         return *this;
     }
 
     template <class T>
     Module& operator()(char const *name, Class<T>& clss) {
-        func_->InstanceTemplate()->
-            Set(v8::String::New(name),
+        obj_->Set(v8::String::New(name),
                 v8::FunctionTemplate::New(
                     &Class<T>::singleton_t::ConstructorFunction));
 
-        clss.FunctionTemplate()->SetClassName(v8::String::New(name));
+        // clss.FunctionTemplate()->SetClassName(v8::String::New(name));
         return *this;
     }
 
     // this is a local handle so make it persistent if needs be
     v8::Local<v8::Object> NewInstance() {
-        return func_->GetFunction()->NewInstance();
+        return obj_->NewInstance();
     }
 
-    Module() : func_(v8::FunctionTemplate::New()) {}
+    Module() : obj_(v8::ObjectTemplate::New()) {}
   private:
-    v8::Local<v8::FunctionTemplate> func_;
+    v8::Local<v8::ObjectTemplate> obj_;
 };
 
 }
