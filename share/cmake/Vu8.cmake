a082f30b7464b256b9eb6e34354a578c6e19bb9d
jpike <jpike@ts-a.com>
2011-01-07 18:40:54 +0000

    Vu8 cmake module can now be used from a superproject

--- share/cmake/Vu8.cmake
+++ share/cmake/Vu8.cmake
@@ -1,3 +1,17 @@
+if(NOT VU8_INCLUDE_DIR)
+    set(VU8_INCLUDE_DIR "${CMAKE_INSTALL_PREFIX}/include")
+endif()
+
+include_directories(${VU8_INCLUDE_DIR})
+if(EXISTS "${VU8_INCLUDE_DIR}/vu8/obj")
+    # within VU8 build system
+    include_directories("${VU8_INCLUDE_DIR}/vu8/obj/v8/include")
+    include_directories("${VU8_INCLUDE_DIR}/vu8/obj/include")
+endif()
+
+# TODO: set VU8_ROOT
+# TODO: set V8_LIB_DYNAMIC
+
 macro(vu8_plugin _name _sources)
     add_library(vu8_${_name} SHARED ${_sources})
     target_link_libraries(vu8_${_name} ${V8_LIB_DYNAMIC})
