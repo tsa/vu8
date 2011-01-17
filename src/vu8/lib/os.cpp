90cf6d65351369cbff2c358f25405f25f638f11c
jpike <jpike@ts-a.com>
2011-01-17 15:53:48 +0000

    call make rather than gmake and provide os module with getenv and kill

---
+++ src/vu8/lib/os.cpp
@@ -0,0 +1,37 @@
+#include <vu8/Class.hpp>
+#include <vu8/Module.hpp>
+
+#include <boost/multi_index/member.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <cstdio>
+
+#include <sys/stat.h>
+#include <signal.h>
+
+namespace vu8 { namespace os {
+
+char const *Getenv(char const *name) {
+    return std::getenv(name);
+}
+
+bool Signal(int const pid, int const signal) {
+    return ! ::kill(pid, signal);
+}
+
+static inline v8::Handle<v8::Value> Open() {
+    v8::HandleScope scope;
+    Module mod;
+    return mod
+        .Set<char const *(char const *), &Getenv>("getenv")
+        .Set<bool(int const, int const), &Signal>("signal")
+        .NewInstance();
+}
+
+} }
+
+extern "C" {
+    v8::Handle<v8::Value> vu8_module_init() { return vu8::os::Open(); }
+}
