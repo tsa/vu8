7c4c24be0355338a2943888e60c642fa59ed6071
jpike <jpike@ts-a.com>
2012-01-25 12:14:15 +0000

    build system simplifications, fix -I

--- share/cmake/FindVu8.cmake
+++ share/cmake/FindVu8.cmake
@@ -26,7 +26,7 @@ endif()
 
 macro(vu8_plugin _name _sources)
     add_library(vu8_${_name} SHARED ${_sources})
-    target_link_libraries(vu8_${_name} ${V8_LIB_DYNAMIC})
+    target_link_libraries(vu8_${_name} vu8)
     if(VU8_INTERNAL_BUILD AND NOT OWN_V8)
         add_dependencies(vu8_${_name} buildextv8)
     endif()
