6894587a8cbf4945d7425b9944940b03fa8d7c9d
hammonda <david.hammond@sannopoint.com>
2012-11-03 09:50:12 +0000

    Changed typename to class to be consistent with parent vu8 project.

--- src/vu8/detail/TypeTraits.hpp
+++ src/vu8/detail/TypeTraits.hpp
@@ -15,24 +15,24 @@
 
 namespace vu8 {
 
-template <typename T> struct remove_reference_and_const {
-  typedef typename boost::remove_const<
-    typename boost::remove_reference<T>::type
-  >::type type;
+template <class T> struct remove_reference_and_const {
+    typedef typename boost::remove_const<
+        typename boost::remove_reference<T>::type
+    >::type type;
 };
 
-template <typename T, typename Enable = void>
+template <class T, class Enable = void>
 struct is_to_v8_convertible : public boost::false_type { };
 
-template <typename T>
+template <class T>
 struct is_to_v8_convertible<T, typename boost::enable_if<
-  boost::is_arithmetic<T>
+    boost::is_arithmetic<T>
 >::type> : public boost::true_type { };
 
 #define VU8_TO_V8_CONV_TYPE_TRAIT_SPEC(T,M,spec) \
-template <typename T > \
+template <class T > \
 struct is_to_v8_convertible<T, typename boost::enable_if< \
-  boost::is_same<M, spec > \
+    boost::is_same<M, spec > \
 >::type> : public boost::true_type { }; \
 /**/
 
